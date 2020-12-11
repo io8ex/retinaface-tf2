9263d6d53eb665d9296599be1294a31a84fb0f4a
io8ex <48648246+io8ex@users.noreply.github.com>
2020-12-11 14:28:46 +0300

    Update test.py

--- test.py
+++ test.py
@@ -11,10 +11,10 @@ from modules.utils import (set_memory_growth, load_yaml, draw_bbox_landm,
                            pad_input_image, recover_pad_output)
 
 
-flags.DEFINE_string('cfg_path', './configs/retinaface_res50.yaml',
+flags.DEFINE_string('cfg_path', './configs/retinaface_mbv2.yaml',
                     'config file path')
 flags.DEFINE_string('gpu', '0', 'which gpu to use')
-flags.DEFINE_string('img_path', '', 'path to input image')
+flags.DEFINE_string('img_path', './data/0_Parade_marchingband_1_149.jpg', 'path to input image')
 flags.DEFINE_boolean('webcam', False, 'get image source from webcam or not')
 flags.DEFINE_float('iou_th', 0.4, 'iou threshold for nms')
 flags.DEFINE_float('score_th', 0.5, 'score threshold for nms')
@@ -23,19 +23,29 @@ flags.DEFINE_float('down_scale_factor', 1.0, 'down-scale factor for inputs')
 
 def main(_argv):
     # init
+
+    CONFIG_PATH = './configs/retinaface_mbv2.yaml'
+    GPU = '0'
+    IOU_TH = 0.4
+    SCORE_TH = 0.5
+    WEBCAM = False
+    IMG_PATH = './data/0_Parade_marchingband_1_149.jpg'
+    DOWN_FACTOR = 1.0
+
     os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-    os.environ['CUDA_VISIBLE_DEVICES'] = FLAGS.gpu
+    os.environ['CUDA_VISIBLE_DEVICES'] = GPU
+
 
     logger = tf.get_logger()
     logger.disabled = True
     logger.setLevel(logging.FATAL)
     set_memory_growth()
 
-    cfg = load_yaml(FLAGS.cfg_path)
+    cfg = load_yaml(CONFIG_PATH)
 
     # define network
-    model = RetinaFaceModel(cfg, training=False, iou_th=FLAGS.iou_th,
-                            score_th=FLAGS.score_th)
+    model = RetinaFaceModel(cfg, training=False, iou_th=IOU_TH,
+                            score_th=SCORE_TH)
 
     # load checkpoint
     checkpoint_dir = './checkpoints/' + cfg['sub_name']
@@ -48,20 +58,20 @@ def main(_argv):
         print("[*] Cannot find ckpt from {}.".format(checkpoint_dir))
         exit()
 
-    if not FLAGS.webcam:
-        if not os.path.exists(FLAGS.img_path):
-            print(f"cannot find image path from {FLAGS.img_path}")
+    if not WEBCAM:
+        if not os.path.exists(IMG_PATH):
+            print(f"cannot find image path from {IMG_PATH}")
             exit()
 
-        print("[*] Processing on single image {}".format(FLAGS.img_path))
+        print("[*] Processing on single image {}".format(IMG_PATH))
 
-        img_raw = cv2.imread(FLAGS.img_path)
+        img_raw = cv2.imread(IMG_PATH)
         img_height_raw, img_width_raw, _ = img_raw.shape
         img = np.float32(img_raw.copy())
 
-        if FLAGS.down_scale_factor < 1.0:
-            img = cv2.resize(img, (0, 0), fx=FLAGS.down_scale_factor,
-                             fy=FLAGS.down_scale_factor,
+        if DOWN_FACTOR < 1.0:
+            img = cv2.resize(img, (0, 0), fx=DOWN_FACTOR,
+                             fy=DOWN_FACTOR,
                              interpolation=cv2.INTER_LINEAR)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
@@ -75,7 +85,7 @@ def main(_argv):
         outputs = recover_pad_output(outputs, pad_params)
 
         # draw and save results
-        save_img_path = os.path.join('out_' + os.path.basename(FLAGS.img_path))
+        save_img_path = os.path.join('out_' + os.path.basename(IMG_PATH))
         for prior_index in range(len(outputs)):
             draw_bbox_landm(img_raw, outputs[prior_index], img_height_raw,
                             img_width_raw)
@@ -93,9 +103,9 @@ def main(_argv):
 
             frame_height, frame_width, _ = frame.shape
             img = np.float32(frame.copy())
-            if FLAGS.down_scale_factor < 1.0:
-                img = cv2.resize(img, (0, 0), fx=FLAGS.down_scale_factor,
-                                 fy=FLAGS.down_scale_factor,
+            if DOWN_FACTOR < 1.0:
+                img = cv2.resize(img, (0, 0), fx=DOWN_FACTOR,
+                                 fy=DOWN_FACTOR,
                                  interpolation=cv2.INTER_LINEAR)
             img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
@@ -108,7 +118,7 @@ def main(_argv):
             # recover padding effect
             outputs = recover_pad_output(outputs, pad_params)
 
-            # draw results
+# draw results
             for prior_index in range(len(outputs)):
                 draw_bbox_landm(frame, outputs[prior_index], frame_height,
                                 frame_width)
@@ -123,12 +133,10 @@ def main(_argv):
             cv2.imshow('frame', frame)
             if cv2.waitKey(1) == ord('q'):
                 exit()
-                
-  #return output with bboxes
-  return outputs
+    return outputs
 
 
-if __name__ == '__main__':
+if name == '__main__':
     try:
         app.run(main)
     except SystemExit:
